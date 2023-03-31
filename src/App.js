bfe73364a80d10bfc92a37a19ee6baab7c687c7d
ogonz801 <g.osvaldo96@gmail.com>
2023-03-30 22:39:18 -0600

    feat: added Header and Footer components as well as a styled component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
+import Header from "./components/ui/Header";
+import Footer from "./components/ui/Footer";
+
 import Home from "./pages/Home";
 import CreateForm from "./pages/CreateForm";
 import DetailPage from "./pages/DetailPage";
 import EditForm from "./pages/EditForm";
 
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  background-color: #1f2833;
+  color: #c5c6c7;
+`;
+
 function App() {
   return (
-    <>
+    <AppContainer>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<CreateForm />} />
         <Route path="/product/:productId" element={<DetailPage />} />
         <Route path="/product/:productId/edit" element={<EditForm />} />
       </Routes>
-    </>
+      <Footer />
+    </AppContainer>
   );
 }
 
