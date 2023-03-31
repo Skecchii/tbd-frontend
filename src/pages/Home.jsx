7acbfff2d7cd371526842a882888ccc4f85d76ea
ogonz801 <g.osvaldo96@gmail.com>
2023-03-30 23:26:26 -0600

    refactor: updated Home page to include new components and a styled component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import Products from "../components/Products";
+import ProductsCategory from "../components/ProductsCategory";
+import SlideShow from "../components/SlideShow";
+
+import styled from "styled-components"
+
+const HeaderContainer = styled.h2`
+  display: flex;
+  justify-content: center;
+`
 
 const Home = () => {
   return (
     <main>
-      <h1>Products</h1>
+      <SlideShow/>
+      <HeaderContainer>Featured Items</HeaderContainer>
       <Products />
+      <HeaderContainer>Shop by Categories</HeaderContainer>
+      <ProductsCategory/>
     </main>
   );
 };
