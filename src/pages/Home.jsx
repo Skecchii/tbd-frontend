dc757edb1c8e49d765ce924826501c4dda4a31bc
ogonz801 <g.osvaldo96@gmail.com>
2023-03-31 21:18:03 -0600

    refactor: removed SlideShow component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Products from "../components/Products";
 import ProductsCategory from "../components/ProductsCategory";
-import SlideShow from "../components/SlideShow";
 
 import styled from "styled-components"
 
@@ -13,7 +12,6 @@ const HeaderContainer = styled.h2`
 const Home = () => {
   return (
     <main>
-      <SlideShow/>
       <HeaderContainer>Featured Items</HeaderContainer>
       <Products />
       <HeaderContainer>Shop by Categories</HeaderContainer>
