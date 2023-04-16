c6b2ae2c200a5a5c7193643c49022fcc599c1c27
ogonz801 <g.osvaldo96@gmail.com>
2023-04-16 17:29:08 -0600

    feat: added global loading logic

--- src/App.js
+++ src/App.js
@@ -1,32 +1,48 @@
-import React from "react";
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/ui/Header";
 import Footer from "./components/ui/Footer";
+import LoadingComponent from "./components/LoadingComponent";
 
 import Home from "./pages/Home";
 import CreateForm from "./pages/CreateForm";
 import DetailPage from "./pages/DetailPage";
 import EditForm from "./pages/EditForm";
+import CartPage from "./pages/CartPage";
 
 import styled from "styled-components";
 
 const AppContainer = styled.div`
   background-color: #1f2833;
   color: #c5c6c7;
+  height: 100vh;
 `;
 
 function App() {
+  const [loading, setLoading] = useState(true)
+
+  const handleLoading = () => {
+    setLoading(false)
+  }
+
   return (
     <AppContainer>
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create" element={<CreateForm />} />
-        <Route path="/product/:productId" element={<DetailPage />} />
-        <Route path="/product/:productId/edit" element={<EditForm />} />
-      </Routes>
-      <Footer />
+      {loading ? (
+        <LoadingComponent handleLoading={handleLoading} />
+      ) : (
+        <>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<CreateForm />} />
+            <Route path="/product/:productId" element={<DetailPage />} />
+            <Route path="/product/:productId/edit" element={<EditForm />} />
+            <Route path="/cart" element={<CartPage />} />
+          </Routes>
+          <Footer />
+        </>
+      )}
     </AppContainer>
   );
 }
