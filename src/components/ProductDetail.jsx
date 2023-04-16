16d41b4a411713b7b025f8dba17f35220a8bb0f1
ogonz801 <g.osvaldo96@gmail.com>
2023-04-16 17:17:32 -0600

    feat: added button to add product to cart

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -2,6 +2,14 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts, deleteExistingProduct } from "../store/productsSlice";
 import { useParams, useNavigate, Link } from "react-router-dom";
+import { addItem } from "../store/cartSlice"
+
+import styled from "styled-components"
+
+const Img = styled.img`
+  max-width: 100%;
+  max-height: 50rem;
+`
 
 function ProductDetail() {
   const { productId } = useParams();
@@ -19,17 +27,24 @@ function ProductDetail() {
     setTimeout(() => navigate("/"), 150)
   }
 
+  const handleAddToCart = () => {
+    dispatch(addItem(product));
+  };
+
   return (
     <main>
-      <h2>{product.name}</h2>
-      <p>{product.description}</p>
-      <p>Price: {product.price}</p>
-      <p>Category: {product.category}</p>
-      <img src={product.imageUrl} alt={product.name} />
-      <Link to={`/product/${product._id}/edit`}>
-        <button>Edit</button>
-      </Link>
+      <>
+        <h2>{product.name}</h2>
+        <p>{product.description}</p>
+        <p>Price: {product.price}</p>
+        <p>Category: {product.category}</p>
+        <Img src={product.imageUrl} alt={product.name} />
+        <Link to={`/product/${product._id}/edit`}>
+          <button>Edit</button>
+        </Link>
         <button onClick={handleDelete}>Delete</button>
+        <button onClick={handleAddToCart}>Add to Cart</button>
+      </>
     </main>
   );
 }
