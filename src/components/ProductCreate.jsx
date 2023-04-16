6e62c5be289ca7383bd467532c3a356fc07131e8
ogonz801 <g.osvaldo96@gmail.com>
2023-04-16 17:15:39 -0600

    feat: feature to upload and change image when creating or updating product

--- src/components/ProductCreate.jsx
+++ src/components/ProductCreate.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { createNewProduct } from "../store/productsSlice";
+import { createNewProduct, uploadImage } from "../store/productsSlice";
 import { useNavigate } from "react-router-dom";
 
 function ProductCreate() {
@@ -16,85 +16,99 @@ function ProductCreate() {
     relatedProduct: [],
   });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    dispatch(createNewProduct(formData));
-    setFormData({
-      name: "",
-      price: "",
-      description: "",
-      category: "",
-      imageUrl: "",
-      relatedProduct: [],
-    });
-    setTimeout(() => navigate("/"), 150);
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      await dispatch(createNewProduct(formData));
+      setFormData({
+        name: "",
+        price: "",
+        description: "",
+        category: "",
+        imageUrl: "",
+        relatedProduct: [],
+      });
+      setTimeout(() => navigate("/"), 150);
+    } catch (err) {
+      console.error(`failed to submit new product, ${err}`);
+    }
   };
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+  const handleChange = async (e) => {
+    try {
+      if (e.target.name === "imageUrl") {
+        const file = e.target.files[0];
+        const newFormData = new FormData();
+        newFormData.append('imageUrl', file);
+        const response = await dispatch(uploadImage(newFormData))
+        setFormData({ ...formData, imageUrl: response.payload })
+      } else {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+      }
+    } catch (err) {
+      console.error(`failed to change formData, ${err}`);
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="name">Name</label>
-      <input
-        type="text"
-        id="name"
-        name="name"
-        value={formData.name}
-        onChange={handleChange}
-        required
-      />
+    <>
+      <button onClick={() => console.log(`current FormData: ${JSON.stringify(formData)}`)}>check data</button>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
 
-      <label htmlFor="price">Price</label>
-      <input
-        type="number"
-        id="price"
-        name="price"
-        value={formData.price}
-        onChange={handleChange}
-        required
-      />
+        <label htmlFor="price">Price</label>
+        <input
+          type="number"
+          id="price"
+          name="price"
+          value={formData.price}
+          onChange={handleChange}
+          required
+        />
 
-      <label htmlFor="description">Description</label>
-      <textarea
-        id="description"
-        name="description"
-        value={formData.description}
-        onChange={handleChange}
-        required
-      />
+        <label htmlFor="description">Description</label>
+        <textarea
+          id="description"
+          name="description"
+          value={formData.description}
+          onChange={handleChange}
+          required
+        />
 
-      <label htmlFor="category">Category</label>
-      <select
-        id="category"
-        name="category"
-        value={formData.category}
-        onChange={handleChange}
-        required
-      >
-        <option value="">--Select a category--</option>
-        <option value="engine">Engine</option>
-        <option value="transmission">Transmission</option>
-        <option value="suspension">Suspension</option>
-        <option value="brakes">Brakes</option>
-        <option value="exhaust">Exhaust</option>
-        <option value="interior">Interior</option>
-        <option value="exterior">Exterior</option>
-      </select>
+        <label htmlFor="category">Category</label>
+        <select
+          id="category"
+          name="category"
+          value={formData.category}
+          onChange={handleChange}
+          required
+        >
+          <option value="">--Select a category--</option>
+          <option value="kissCut">Kiss Cut</option>
+          <option value="peeker">Peeker</option>
+          <option value="boxSlap">Box Slap</option>
+        </select>
 
-      <label htmlFor="imageUrl">Image URL</label>
-      <input
-        type="text"
-        id="imageUrl"
-        name="imageUrl"
-        value={formData.imageUrl}
-        onChange={handleChange}
-        required
-      />
+        <label htmlFor="imageUrl">Upload Image</label>
+        <input
+          type="file"
+          id="imageUrl"
+          name="imageUrl"
+          onChange={handleChange}
+        />
 
-      <button type="submit">Create Product</button>
-    </form>
+        <button type="submit">Create Product</button>
+      </form>
+    </>
+
   );
 }
 
--- src/components/ProductEdit.jsx
+++ src/components/ProductEdit.jsx
@@ -1,7 +1,104 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { updateExistingProduct } from "../store/productsSlice";
-import { useNavigate, useParams } from "react-router-dom";
+import { updateExistingProduct, uploadImage } from "../store/productsSlice";
+import { useNavigate, useParams, Link } from "react-router-dom";
+import styled from "styled-components";
+
+const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.5rem;
+  max-width: 800px;
+  margin: 0 auto;
+`;
+
+const FormGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+  width: 100%;
+`;
+
+const Img = styled.img`
+  max-width: 100%;
+  max-height: 50rem;
+`
+
+const Label = styled.label`
+  color: #fff;
+  font-size: 1rem;
+`;
+
+const Input = styled.input`
+  border: none;
+  border-radius: 5px;
+  padding: 0.5rem;
+  font-size: 1rem;
+  width: 100%;
+`;
+
+const Textarea = styled.textarea`
+  border: none;
+  border-radius: 5px;
+  padding: 0.5rem;
+  font-size: 1rem;
+  height: 200px;
+  width: 100%;
+  resize: vertical;
+`;
+
+const Select = styled.select`
+  border: none;
+  border-radius: 5px;
+  padding: 0.5rem;
+  font-size: 1rem;
+  background-color: #f0f0f0;
+  color: #333;
+  width: 100%;
+`;
+
+const Option = styled.option`
+  background-color: #f0f0f0;
+  color: #333;
+  transition: 0.2s;
+  &:hover {
+    background-color: #66fcf1;
+    color: #000;
+    cursor: pointer;
+  }
+`;
+
+const Button = styled.button`
+  background-color: #1f2833;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  padding: 0.5rem;
+  font-size: 1rem;
+  cursor: pointer;
+  transition: 0.2s;
+  &:hover {
+    background-color: #66fcf1;
+    color: #333;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #1f2833;
+  background-color: #1f2833;
+  border: none;
+  border-radius: 5px;
+  padding: 0.5rem;
+  font-size: 1rem;
+  cursor: pointer;
+  transition: 0.2s;
+  &:hover {
+    color: #66fcf1;
+  }
+`;
 
 function ProductEdit() {
   const { productId } = useParams();
@@ -19,6 +116,8 @@ function ProductEdit() {
     relatedProduct: [],
   });
 
+  const [newImage, setNewImage] = useState(null)
+
   useEffect(() => {
     if (product) {
       setFormData({
@@ -34,83 +133,123 @@ function ProductEdit() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(updateExistingProduct({productId, productData: formData}));
-    setFormData({
-      name: "",
-      price: "",
-      description: "",
-      category: "",
-      imageUrl: "",
-      relatedProduct: [],
-    });
-    setTimeout(() => navigate(`/product/${productId}`), 150);
+    if (newImage) {
+      formData.imageUrl = newImage;
+      dispatch(updateExistingProduct({ productId, productData: formData }))
+      setFormData({
+        name: "",
+        price: "",
+        description: "",
+        category: "",
+        imageUrl: "",
+        relatedProduct: [],
+      });
+      setNewImage(null)
+      console.log(formData)
+      setTimeout(() => navigate(`/product/${productId}`), 250);
+    } else {
+      dispatch(updateExistingProduct({ productId, productData: formData }));
+      setFormData({
+        name: "",
+        price: "",
+        description: "",
+        category: "",
+        imageUrl: "",
+        relatedProduct: [],
+      });
+      setTimeout(() => navigate(`/product/${productId}`), 250);
+    }
   };
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+  const handleChange = async (e) => {
+    if (e.target.name === "imageUrl") {
+      const file = e.target.files[0];
+      const newFormData = new FormData();
+      newFormData.append('imageUrl', file);
+      const response = await dispatch(uploadImage(newFormData))
+      setNewImage(response.payload)
+    } else {
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+    }
   };
+
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="name">Name</label>
-      <input
-        type="text"
-        id="name"
-        name="name"
-        value={formData.name}
-        onChange={handleChange}
-        required
-      />
+    <>
+      <button onClick={() => console.log(newImage)}>PRESS ME</button>
+      <FormContainer onSubmit={handleSubmit}>
+        <FormGroup>
+          <Label htmlFor="name">Name</Label>
+          <Input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
 
-      <label htmlFor="price">Price</label>
-      <input
-        type="number"
-        id="price"
-        name="price"
-        value={formData.price}
-        onChange={handleChange}
-        required
-      />
+          <Label htmlFor="price">Price</Label>
+          <Input
+            type="number"
+            id="price"
+            name="price"
+            value={formData.price}
+            onChange={handleChange}
+            required
+          />
 
-      <label htmlFor="description">Description</label>
-      <textarea
-        id="description"
-        name="description"
-        value={formData.description}
-        onChange={handleChange}
-        required
-      />
+          <Label htmlFor="description">Description</Label>
+          <Textarea
+            id="description"
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+            required
+          />
 
-      <label htmlFor="category">Category</label>
-      <select
-        id="category"
-        name="category"
-        value={formData.category}
-        onChange={handleChange}
-        required
-      >
-        <option value="">--Select a category--</option>
-        <option value="engine">Engine</option>
-        <option value="transmission">Transmission</option>
-        <option value="suspension">Suspension</option>
-        <option value="brakes">Brakes</option>
-        <option value="exhaust">Exhaust</option>
-        <option value="interior">Interior</option>
-        <option value="exterior">Exterior</option>
-      </select>
+          <Label htmlFor="category">Category</Label>
+          <Select
+            id="category"
+            name="category"
+            value={formData.category}
+            onChange={handleChange}
+            required
+          >
+            <Option value="">--Select a category--</Option>
+            <Option value="kissCut">Kiss Cut</Option>
+            <Option value="peeker">Peeker</Option>
+            <Option value="boxSlap">Box Slap</Option>
+          </Select>
 
-      <label htmlFor="imageUrl">Image URL</label>
-      <input
-        type="text"
-        id="imageUrl"
-        name="imageUrl"
-        value={formData.imageUrl}
-        onChange={handleChange}
-        required
-      />
-
-      <button type="submit">Update Product</button>
-    </form>
+          <Label htmlFor="imageUrl">Image</Label>
+          {formData.imageUrl ? (
+            <div>
+              <Img src={formData.imageUrl} alt={formData.name} />
+              <label htmlFor="imageUrl">Change Image</label>
+              <Input
+                type="file"
+                id="imageUrl"
+                name="imageUrl"
+                onChange={handleChange}
+                style={{ display: "none" }}
+              />
+            </div>
+          ) : (
+            <Input
+              type="file"
+              id="imageUrl"
+              name="imageUrl"
+              onChange={handleChange}
+            />
+          )}
+        </FormGroup>
+        <div>
+          <Button type="submit">Update Product</Button>
+          <StyledLink to={`/product/${productId}`}>Cancel</StyledLink>
+        </div>
+      </FormContainer>
+    </>
   );
 }
 
